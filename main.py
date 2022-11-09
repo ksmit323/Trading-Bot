0eed6e37db2074bc6f1744e59340894dc0c359f5
Kenneth Smith <ksmit323@gmail.com>
2022-11-09 18:20:42 +0700

    adding the file test

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy
+import pandas
+import matplotlib
+import backtesting
+import talib
+
+#* Balls
+#! Shit balls!
+#? Balls?
+# TODO: oh balls
+
+def main():
+    ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
