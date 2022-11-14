c302fa649adaf1c68d2dc961e1e497d7f7e23129
ksmit323 <106971980+ksmit323@users.noreply.github.com>
2022-11-14 20:29:14 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy
+import pandas
+import matplotlib
+import backtesting
+import talib
+
+#* Balls
+#! Shit balls!
+#? Balls?
+# TODO: oh balls
+
+def main():
+    ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
