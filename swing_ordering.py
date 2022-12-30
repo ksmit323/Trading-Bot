161e47a6e94b967a8943d79970c3886270bd3e57
ksmit323 <106971980+ksmit323@users.noreply.github.com>
2022-12-30 10:33:16 +0700

    Create swing_ordering.py

---
+++ swing_ordering.py
@@ -0,0 +1,153 @@
+from ib_insync import *
+import sys
+
+
+# Instantiate IB class and establish connection
+ib = IB()
+# * Change port id when on live account to 7496
+ib.connect('127.0.0.1', 7497, 1)
+if ib.isConnected():
+    print("Connection established")
+else:
+    print("Failed to connect")
+
+
+def main():
+
+    tickers = []
+
+    for ticker in tickers:
+
+
+        # Create a contract
+        contract = Stock(ticker, "SMART", "USD")
+        ib.qualifyContracts(contract)
+        df = build_dataframe(contract)
+
+        stop_limit = round((df.high.iloc[-1] + 0.01), 2)
+        limit_price = round((df.high.iloc[-1] + 0.02), 2)
+        stop_loss = round((df.low.iloc[-1] - 0.01), 2)
+        risk_per_share = round((limit_price - stop_loss), 2)
+        take_profit_increment = risk_per_share * 2
+        take_profit_level = round(
+            (df.high.iloc[-1] + take_profit_increment), 2)
+
+        quantity = share_size(risk_per_share)
+
+        place_order(
+            contract,
+            "BUY",
+            quantity,
+            limit_price,
+            take_profit_level,
+            take_profit_increment,
+            stop_loss,
+            stop_limit
+        )
+        print(f"Order for {ticker} has been placed")
+
+
+def build_dataframe(contract):
+    """ Returns a dataframe of weekly bars from a contract argument """
+
+    # Request live updates for historical bars
+    bars = ib.reqHistoricalData(
+        contract,
+        endDateTime="",
+        durationStr="5 W",          # Change to "5 W" for the weekly, "5 D" for daily
+        barSizeSetting="1 week",     # Change to "1 week" for weekly, "1 day" for the daily
+        whatToShow="TRADES",
+        useRTH=True,
+        formatDate=1,
+        keepUpToDate=True)
+
+    # Build dataframe from bars data list
+    df = util.df(bars)
+
+    return df
+
+
+def place_order(contract, action: str,
+                quantity: int,
+                limit_price: float,
+                take_profit_limit_price: float,
+                take_profit_increment: float,
+                stop_loss_price: float,
+                stop_limit: float):
+    """ 
+    This function handles all of the order placements into IB.
+    It uses a bracket order to incorporate profit taking and 
+    a stop loss.  
+
+    The take_profit object includes scale attributes so that
+    profit prices can vary depending on the level size.
+    The scaleInitLevelSize is the first scaling out profit level of 
+    the position.  The scaleSubsLevelSize is the next portion that
+    the bracket order will sell at the next profit level. 
+    The scalePriceIncrement is how much the profit level will
+    increase up at each sell point.   
+
+    NOTE: Make sure to always handle order transmission accurately.
+    The transmit attribute for all orders should be set to False
+    except for the last order, which is of course set to True.
+    """
+    # Get an order ID to assign to the parent order
+    parent_order_id = ib.client.getReqId()
+
+    # Parent Stop Limit order
+    parent = Order()
+    parent.orderId = parent_order_id
+    parent.action = action
+    parent.orderType = "STP LMT"
+    parent.totalQuantity = quantity
+    parent.lmtPrice = limit_price
+    parent.auxPrice = stop_limit
+    parent.tif = "GTC"
+    parent.outsideRth = True
+    parent.transmit = False
+
+    # Profit taking order
+    take_profit = Order()
+    take_profit.orderId = parent.orderId + 1
+    take_profit.action = "SELL" if action == "BUY" else "BUY"
+    take_profit.orderType = "LMT"
+    take_profit.totalQuantity = quantity
+    take_profit.lmtPrice = take_profit_limit_price
+    take_profit.scaleInitLevelSize = quantity // 3
+    take_profit.scaleSubsLevelSize = quantity // 4
+    take_profit.scalePriceIncrement = take_profit_increment
+    take_profit.parentId = parent_order_id
+    take_profit.tif = "GTC"
+    take_profit.outsideRth = True
+    take_profit.transmit = False
+
+    # Stop loss order
+    stop_loss = Order()
+    stop_loss.orderId = parent.orderId + 2
+    stop_loss.action = "SELL" if action == "BUY" else "BUY"
+    stop_loss.orderType = "STP"
+    stop_loss.auxPrice = stop_loss_price
+    stop_loss.totalQuantity = quantity
+    stop_loss.parentId = parent_order_id
+    stop_loss.tif = "GTC"
+    stop_loss.transmit = True
+
+    bracket_order = [parent, take_profit, stop_loss]
+
+    for order in bracket_order:
+        ib.placeOrder(contract, order)
+
+
+def share_size(risk_per_share):
+    """ 
+    Function will determine the correct position sizing
+    based on risk amount, account size and risk per share
+    """
+    account_size = 1700
+    risk_percent = 0.02
+    shares = round((account_size * risk_percent) / risk_per_share)
+    return shares
+
+
+if __name__ == '__main__':
+    main()
