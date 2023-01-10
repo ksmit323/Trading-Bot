c3f6b30355a37cf5789b48f1cd5c196a4c78fdee
ksmit323 <106971980+ksmit323@users.noreply.github.com>
2023-01-10 11:40:15 +0700

    Update swing_ordering.py

--- swing_ordering.py
+++ swing_ordering.py
@@ -4,8 +4,8 @@ import sys
 
 # Instantiate IB class and establish connection
 ib = IB()
-# * Change port id when on live account to 7496
-ib.connect('127.0.0.1', 7497, 1)
+# * Change port id when on live account to 7496, Paper account to 7497 
+ib.connect('127.0.0.1', 7496, 1)
 if ib.isConnected():
     print("Connection established")
 else:
@@ -14,38 +14,58 @@ else:
 
 def main():
 
+    place_orders = True
+
+    if not place_orders:
+        adjust_stop_losses()
+        print("Stop losses have been updated")
+
     tickers = []
 
-    for ticker in tickers:
+    position_value = 0
+    
+    if place_orders:
 
+        for ticker in tickers:
 
-        # Create a contract
-        contract = Stock(ticker, "SMART", "USD")
-        ib.qualifyContracts(contract)
-        df = build_dataframe(contract)
+            # Create a contract
+            contract = Stock(ticker, "SMART", "USD")
+            ib.qualifyContracts(contract)
+            df = build_dataframe(contract)
 
-        stop_limit = round((df.high.iloc[-1] + 0.01), 2)
-        limit_price = round((df.high.iloc[-1] + 0.02), 2)
-        stop_loss = round((df.low.iloc[-1] - 0.01), 2)
-        risk_per_share = round((limit_price - stop_loss), 2)
-        take_profit_increment = risk_per_share * 2
-        take_profit_level = round(
-            (df.high.iloc[-1] + take_profit_increment), 2)
+            # Set stop limit prices
+            if df.open.iloc[-1] < 1:
+                stop_limit = round((df.high.iloc[-1] + 0.005), 3)
+            else:
+                stop_limit = round((df.high.iloc[-1] + 0.01), 2)
+            limit_price = round((df.high.iloc[-1] + 0.02), 2)
 
-        quantity = share_size(risk_per_share)
+            # Set stop loss
+            if df.open.iloc[-1] < 1:
+                stop_loss = round((df.low.iloc[-1] - 0.005), 3)
+            else:
+                stop_loss = round((df.low.iloc[-1] - 0.01), 2)
 
-        place_order(
-            contract,
-            "BUY",
-            quantity,
-            limit_price,
-            take_profit_level,
-            take_profit_increment,
-            stop_loss,
-            stop_limit
-        )
-        print(f"Order for {ticker} has been placed")
+            risk_per_share = round((limit_price - stop_loss), 2)
+            take_profit_increment = risk_per_share
+            take_profit_level = round(
+                (df.high.iloc[-1] + take_profit_increment), 2)
+            quantity = share_size(risk_per_share)
 
+            place_order(
+                contract,
+                "BUY",
+                quantity,
+                limit_price,
+                take_profit_level,
+                take_profit_increment,
+                stop_loss,
+                stop_limit
+            )
+            print(f"Order for {ticker} has been placed")
+            position_value += round(stop_limit * quantity)
+            
+        print(f"Total position value: ${position_value}")
 
 def build_dataframe(contract):
     """ Returns a dataframe of weekly bars from a contract argument """
@@ -54,8 +74,8 @@ def build_dataframe(contract):
     bars = ib.reqHistoricalData(
         contract,
         endDateTime="",
-        durationStr="5 W",          # Change to "5 W" for the weekly, "5 D" for daily
-        barSizeSetting="1 week",     # Change to "1 week" for weekly, "1 day" for the daily
+        durationStr= "5 D",          # Change to "5 W" for the weekly, "5 D" for daily
+        barSizeSetting= "1 day",     # Change to "1 week" for weekly, "1 day" for the daily
         whatToShow="TRADES",
         useRTH=True,
         formatDate=1,
@@ -103,7 +123,7 @@ def place_order(contract, action: str,
     parent.lmtPrice = limit_price
     parent.auxPrice = stop_limit
     parent.tif = "GTC"
-    parent.outsideRth = True
+    #parent.outsideRth = True
     parent.transmit = False
 
     # Profit taking order
@@ -138,13 +158,40 @@ def place_order(contract, action: str,
         ib.placeOrder(contract, order)
 
 
+def adjust_stop_losses():                                               
+    """ Function will update all hourly stop losses """                                                
+
+    for trade in ib.openTrades():
+
+        if trade.order.orderType == "STP":
+
+            # Create new contract based on the symbol
+            contract = Stock(trade.contract.symbol, "SMART", "USD")
+            ib.qualifyContracts(contract)
+
+            # Make sure the bars data is up to date
+            df = build_dataframe(contract)
+
+            # Replace previous stop order with new price
+            try:
+                if df.low.iloc[-2] < 1:
+                    trade.order.auxPrice = round((df.low.iloc[-1] - 0.005), 3)
+                else:
+                    trade.order.auxPrice = round((df.low.iloc[-1] - 0.01), 2)
+                ib.placeOrder(contract, trade.order)
+            except AttributeError:
+                print(f"*** WARNING: Stop loss not updated for: {trade.contract.symbol} ***")
+                continue
+            ib.sleep(0.5)
+
+
 def share_size(risk_per_share):
     """ 
     Function will determine the correct position sizing
     based on risk amount, account size and risk per share
     """
-    account_size = 1700
-    risk_percent = 0.02
+    account_size = 600
+    risk_percent = 0.01
     shares = round((account_size * risk_percent) / risk_per_share)
     return shares
 
